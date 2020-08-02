9e22e682396a167807b6ab62c6f8e4e6dce09b60
Shafran <Shafran@ieee.org>
2020-08-02 20:04:36 +0530

    app2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,64 +9,64 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/logo.png'),
-              ),
-              Text(
-                'Shafran Naizer',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Lato'),
-              ),
-              Text(
-                'Flutter Developer',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                height: 5.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.white,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                //padding: EdgeInsets.all(10.0),
-                child: ListTile(
-                  leading: Icon(Icons.phone, color: Colors.teal),
-                  title: Text(
-                    '12u3u33u3u',
-                    style: TextStyle(color: Colors.teal, fontSize: 15.0),
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                //padding: EdgeInsets.all(10.0),
-                child: ListTile(
-                  leading: Icon(Icons.email, color: Colors.teal),
-                  title: Text(
-                    'mshafran13@gmail.com ',
-                    style: TextStyle(color: Colors.teal, fontSize: 15.0),
-                  ),
-                ),
-              ),
-            ],
-          ),
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice Game'),
+          backgroundColor: Colors.red,
         ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  print(leftDiceNum);
+                  changeDiceFace();
+                },
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$leftDiceNum.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$rightDiceNum.png'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
